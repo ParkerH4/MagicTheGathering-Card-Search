09d6c15d054ed40475653e9034c482bc9fa09476
Parker Hewitt <parkerhewitt95@gmail.com>
2023-12-17 17:38:30 -0700

    Adjusted project for Portfolio.

--- client/src/components/CardList.js
+++ client/src/components/CardList.js
@@ -4,12 +4,12 @@ function CardList({ cards }) {
   return (
     <div className="grid grid-cols-4 gap-6 ">
       {cards.map(card => (
-        <div key={card.id} className="border p-4 rounded-md bg-zinc-200">
+        <div key={card.id} className="border p-4 rounded-md bg-zinc-200 transition-transform transform hover:scale-110 hover:bg-blue-700">
           {card.image_uris && (
             <img
               src={card.image_uris.normal}
               alt={card.name}
-              className="w-full mb-4"
+              className="w-full mb-4 rounded-lg hover:shadow-lg group-hover:z-10 "
             />
           )}
           <h2 className="font-bold">Name</h2>
